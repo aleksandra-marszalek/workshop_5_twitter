58e28cf54eef90d5d6e9fc397e2f52c1f0117874
Aleksandra Marszalek <aaleksandra.mmarszalek@gmail.com>
2018-05-11 15:46:49 +0200

    Add home controller

--- src/main/java/pl/coderslab/controller/UserController.java
+++ src/main/java/pl/coderslab/controller/UserController.java
@@ -7,12 +7,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.entity.User;
 import pl.coderslab.service.UserService;
 
 import javax.validation.Valid;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
